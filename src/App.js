99d2a02675e88b56cb8f8d1d91cc9f3256334ff9
Svei <avn.jan5@gmail.com>
2021-01-26 17:02:53 -0600

    finished

---
+++ src/App.js
@@ -0,0 +1,48 @@
+
+import './App.css';
+import Game from './components/game/Game';
+import RaccoonImage from './assets/images/raccoon.png';
+import SquirrelImage from './assets/images/squirrel.png';
+import BunnyImage from './assets/images/bunny.png';
+import HoundImage from './assets/images/hound.png';
+
+
+function App(props) {
+  const raccoons = {
+    name: 'Russiaville Raccoons',
+    logoSrc: RaccoonImage
+  }
+
+  const squirrels = {
+    name: 'Sheridan Squirrels',
+    logoSrc: SquirrelImage
+  }
+
+  const bunnies = {
+    name: 'Burlington Bunnies',
+    logoSrc: BunnyImage
+  }
+
+  const hounds = {
+    name: 'Hammond Hounds',
+    logoSrc: HoundImage
+  }
+
+  return (
+    <div className="App">
+      <Game
+        venue="Union 525 Gem"
+        homeTeam={squirrels}
+        visitingTeam={raccoons}
+      />
+      <Game
+        venue="Sheridan Arena"
+        homeTeam={bunnies}
+        visitingTeam={hounds}
+      />
+    </div>
+  )
+}
+
+
+export default App;
